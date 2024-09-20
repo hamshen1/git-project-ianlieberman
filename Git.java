7d13ad2d0c2018706a4ca00269658bcdef3b6164
hamshen1 <hamshen1@hwemail.com>
2024-09-20 10:43:58 -0700

    Added MessageDigest filename generator
    DOESNT WORK DOESNT WORK DOESNT WORK DOESNT WORK DOESNT WORK DOESNT WORK DOESNT WORK DOESNT WORK DOESNT WORK DOESNT WORK DOESNT WORK DOESNT WORK DOESNT WORK DOESNT WORK DOESNT WORK DOESNT WORK DOESNT WORK DOESNT WORK DOESNT WORK DOESNT WORK DOESNT WORK DOESNT WORK DOESNT WORK DOESNT WORK DOESNT WORK DOESNT WORK DOESNT WORK DOESNT WORK DOESNT WORK DOESNT WORK DOESNT WORK DOESNT WORK DOESNT WORK DOESNT WORK DOESNT WORK DOESNT WORK DOESNT WORK DOESNT WORK DOESNT WORK DOESNT WORK DOESNT WORK DOESNT WORK DOESNT WORK DOESNT WORK DOESNT WORK DOESNT WORK DOESNT WORK

--- Git.java
+++ Git.java
@@ -3,9 +3,10 @@ import java.nio.file;
 
 public class Git
 {
+    
     public static void main(String[]args)
     {
-        
+        MessageDigest md = MessageDigest.getInstance("SHA1");
     }
 
     public static void initRepo() {
@@ -26,4 +27,19 @@ public class Git
             }
         } // Checks if git dir already exists, if not, then makes it
     }
+
+    public String hashFile(File f) {
+        byte[] fContents = Files.readAllBytes(f.getPath());
+
+        return md.digest(fContents).toString();
+    } // Reads out files contents into byte array then hashes it with MessageDigest with SHA-1
+
+    /*References
+     * http://www.sha1-online.com/sha1-java/
+     * https://docs.oracle.com/javase/8/docs/api/java/security/MessageDigest.html
+     * https://stackoverflow.com/questions/858980/file-to-byte-in-java
+     * https://stackoverflow.com/questions/3849692/whole-text-file-to-a-string-in-java
+     * https://docs.oracle.com/en/java/javase/11/docs/api/java.base/java/nio/file/Files.html#readString(java.nio.file.Path)
+     * https://docs.oracle.com/javase/8/docs/api/java/io/File.html
+     */
 }
